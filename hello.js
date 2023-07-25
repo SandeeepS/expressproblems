2b84be97b76193b2d3b5538c77e6d124845ce98e
SandeeepS <sandeepsanthosh3303@gmail.com>
2023-07-25 18:08:42 +0530

    third commit

--- hello.js
+++ hello.js
@@ -2,22 +2,40 @@ const express = require('express');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const app = express();
+const path = require('path');
 const port = 3000;
 //home page
-app.get('/home',(req,res)=>{
+/*app.get('/home',(req,res)=>{
     res.send("home page ");
+})*/
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
 })
+
 //pproduct page
 app.get('/product',(req,res)=>{
-    res.send("product page ");
+    res.sendFile(path.join(__dirname,'product.html'));
 })
-//pproduct page
+
+app.post('/product',(req,res)=>{
+    res.send("product created ");
+})
+
+app.put('/product',(req,res)=>{
+    res.send("product edited ");
+})
+
+app.delete('/product',(req,res)=>{
+    res.send("product deleted ");
+})
+//images page
 app.get('/images',(req,res)=>{
     res.send("images page ");
 })
-//pproduct page
+//about page
 app.get('/about',(req,res)=>{
-    res.send("about page ");
+    res.sendFile(path.join(__dirname,'about.html'));
 })
 app.listen(port,()=>{
     console.log(`app lisented on port:${port}`);
